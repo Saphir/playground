17b88127ca71315e74e3fe5c8c444d9a200a7a47
zhenghaowen <zhenghaowen@momenta.ai>
2022-05-07 01:26:10 +0800

    add protobuf sample code

--- Makefile
+++ Makefile
@@ -1,21 +1,15 @@
 BUILD_DIR := ./build
 MAIN := main
 
-run: run_c run_py
-
-run_c: ${BUILD_DIR}/${MAIN}
-	${BUILD_DIR}/${MAIN}
-
-run_py: ${BUILD_DIR}/src/libtest.so
-	poetry run python3 test_ctypes.py
-
-${BUILD_DIR}/${MAIN}: build
-${BUILD_DIR}/src/libtest.so: build
-
 .PHONY: build clean
 build:
-	[ ! -d build ] || rm -rf build && mkdir build && cd build && cmake .. && make
+	[ ! -d build ] || rm -rf build && mkdir build && cd build && \
+	cmake .. && make
 
 clean:
 	rm -rf $(BUILD_DIR)
 
+	@mkdir -p build && cd build && cmake .. && make
+
+clean:
+	@rm -rf build
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,11 @@
 #include <cstdio>   /* fopen fgets printf */
 #include <cstdlib>  /* exit system malloc atoi rand */
 #include <iostream> /* std::cout std::endl */
+#include "module_cpp/module_cpp.hpp"
+#include "proto/addressbook.pb.h"
 
-#include "test_cpp/test.hpp"
 extern "C" {
-#include "test_c/test.h"
+#include "module_c/module_c.h"
 }
 
 void foo() {
@@ -35,10 +36,35 @@ void foo() {
   test_c_func('a', 1, 2.3, str, (char *)&s1, (char *)&ns, (char *)&out);
 }
 
-void bar() { test_cpp::test_cpp_func(7); }
+void bar() { module_cpp::test_cpp_func(7); }
 
-int main(int argc, char *argv[]) {
-  foo();
-  bar();
-  return EXIT_SUCCESS;
+// Main function:  Reads the entire address book from a file,
+//   adds one person based on user input, then writes it back out to the same
+//   file.
+int main(int argc, char* argv[]) {
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  if (check_main_argc(argc, argv) != 0) {
+    return -1;
+  }
+
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  tutorial::AddressBook address_book;
+
+  if (!module_cpp::ReadAddressBook(&address_book, std::string(argv[1]))) {
+    return -1;
+  }
+
+  // Add an address.
+  module_cpp::PromptForAddress(address_book.add_people());
+
+  if (!module_cpp::WriteAddressBook(address_book, argv[1])) {
+    return -1;
+  }
+
+  // Optional:  Delete all global objects allocated by libprotobuf.
+  google::protobuf::ShutdownProtobufLibrary();
+
+  return 0;
 }
