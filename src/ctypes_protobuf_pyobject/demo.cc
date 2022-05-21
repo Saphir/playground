99d4b6759b2d0b6ee83408ad0320089943d1eb50
Elan Zheng <zhenghaowen@momenta.ai>
2022-05-22 00:06:19 +0800

    pass PyObject *arg to C function

--- src/ctypes_protobuf_pyobject/demo.cc
+++ src/ctypes_protobuf_pyobject/demo.cc
@@ -9,12 +9,13 @@
 extern "C" {
 #endif
 
-PyObject *demo() {
-  tutorial::AddressBook address_book;
+static tutorial::AddressBook generate_address_book(const int64_t id,
+                                                   const std::string &name) {
+  tutorial::AddressBook address_book{};
 
   auto people = address_book.add_people();
-  people->set_id(1);
-  people->set_name("John Doe");
+  people->set_id(id);
+  people->set_name(name);
   people->set_email("john@example.com");
 
   auto phone = people->add_phones();
@@ -24,9 +25,28 @@ PyObject *demo() {
   phone->set_number("555-4322");
   phone->set_type(tutorial::Person::HOME);
 
+  return address_book;
+}
+
+PyObject *demo1(PyObject *arg_id, PyObject *arg_name) {
   std::string serialized;
-  address_book.SerializeToString(&serialized);
+  const long id = PyLong_AsLong(arg_id);
+  const std::string name = PyUnicode_AsUTF8(arg_name);
+  generate_address_book(id, name).SerializeToString(&serialized);
   return PyBytes_FromStringAndSize(serialized.data(), serialized.size());
+}
+
+static PyObject *method_demo(PyObject *self, PyObject *args) {
+  int i = 0;
+  double d = 0.0;
+  char *s = nullptr;
+
+  /* Parse arguments */
+  if (!PyArg_ParseTuple(args, "ids", &i, &d, &s)) {
+    return nullptr;
+  }
+
+  return PyLong_FromLong(1);
 }
 
 #ifdef __cplusplus
--- src/ctypes_protobuf_pyobject/demo.py
+++ src/ctypes_protobuf_pyobject/demo.py
@@ -3,9 +3,11 @@ import ctypes
 from addressbook_pb2 import AddressBook
 
 def _demo():
-    demo = ctypes.cdll.LoadLibrary('./libplayground.so').demo
-    demo.restype = ctypes.py_object
-    s = demo()
+    demo1 = ctypes.cdll.LoadLibrary('./libplayground.so').demo1
+    demo1.argtypes = [ctypes.py_object, ctypes.py_object]
+    demo1.restype = ctypes.py_object
+    s = demo1(7, "Billy")
+    print(type(s))
     addressbook = AddressBook()
     addressbook.ParseFromString(s)
     print(addressbook)
